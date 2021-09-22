940428a26e32f7cd4dbaa6d741ca9d1ca8d26260
Rishik Hombal <hombalrishik@gmail.com>
2021-09-21 22:11:31 -0400

    updated api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const baseURL = "https://rishik.xyz";
-export const getAllExperience = `${baseURL}/experience`;
-export const getAllEducation = `${baseURL}/education`;
-export const getAllProjects = `${baseURL}/projects`;
-export const getAllSkills = `${baseURL}/skills`;
+const BASE_API_URL = "https://rishik.xyz/api";
+export const getAllExperience = `${BASE_API_URL}/experience`;
+export const getAllEducation = `${BASE_API_URL}/education`;
+export const getAllProjects = `${BASE_API_URL}/projects`;
+export const getAllSkills = `${BASE_API_URL}/skills`;
